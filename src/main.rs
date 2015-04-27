1229fa085e3bcdd8c55ddac450be93796268d92e
Hao Wu <hwu@intellisurvey.com>
2015-04-26 21:45:23 -0700

    get it work

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,29 @@
 //#![feature(custom_derive, plugin)]
 //#![plugin(serde_macros)]
-
+#![feature(convert)]  // as str
 extern crate serde;
-
 use serde::json::{self, Value};
+// io
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
-    let data: Value = json::from_str("{\"foo\": 13, \"bar\": \"baz\"}").unwrap();
-    println!("data: {:?}", data);
-    // data: {"bar":"baz","foo":13}
-    println!("object? {}", data.is_object());
-    // object? true
+    let mut json_string = String::new();
+    let mut file = File::open("data.json").unwrap();
+    file.read_to_string(&mut json_string).unwrap();
+    let data: Value = json::from_str(json_string.as_str()).unwrap();
 
-    let obj = data.as_object().unwrap();
-    let foo = obj.get("foo").unwrap();
+    let vec = data.as_array().unwrap();
 
-    println!("array? {:?}", foo.as_array());
-    // array? None
-    println!("u64? {:?}", foo.as_u64());
-    // u64? Some(13u64)
-
-    for (key, value) in obj.iter() {
-        println!("{}: {}", key, match *value {
-            Value::U64(v) => format!("{} (u64)", v),
-            Value::String(ref v) => format!("{} (string)", v),
-            _ => format!("other")
-        });
+    println!("Name, Credit Card");
+    for credit_card in vec {
+        let card =  credit_card.as_object().unwrap();
+        if !card.get("creditcard").unwrap().is_null() {
+        println!("{}, {}",
+                 card.get("name").unwrap().as_string().unwrap(),
+                 card.get("creditcard").unwrap().as_string().unwrap(),
+        );
+        }
     }
     // bar: baz (string)
     // foo: 13 (u64)
